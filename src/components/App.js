8435aa82f4cf158f4c2cd95ede65eaabbaafd9ff
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-28 00:26:32 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 
 import React from "react";
 import './../styles/App.css';
+import '../styles/App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
+  const [taskInput, setTaskInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="master">
+      <div id='main'>
+        <div id="todo">
+          <input type="text" className="input" placeholder="Enter Task" value={taskInput}
+            onChange={((e) => setTaskInput(e.target.value))}
+          />
+          <button className="btn"
+            onClick={() => {
+              setTasks([...tasks, { id: uuidv4(), Name: taskInput }]);
+              setTaskInput("")
+            }
+            }
+          >Add Todo</button>
+        </div>
+        <div id="tasks">
+          {
+            tasks.map(task => <div className="task" key={task.id}><p className="taskName">{task.Name}</p><button className="btn"
+            onClick={() => setTasks(tasks.filter((t) => t.id !== task.id))}
+            
+            >Delete</button></div>)
+          }
+
+        </div>
+      </div>
     </div>
   )
 }
